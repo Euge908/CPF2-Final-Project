3f9b21591131ff45103eba1f8fd4d24f02678245
Euge908 <eugene_cedric_reyes@dlsu.edu.ph>
2021-09-11 17:39:11 +0800

    Working mode input?

--- Timer.v
+++ Timer.v
@@ -33,7 +33,7 @@ Project Features:
 
 /*
 By default, mode is at time and date
-ALSO, time must be in 64 bits to avoid Y2K problem
+ALSO, time must be in 64 bits to avoid Y2038 problem
 */
 
 
@@ -45,53 +45,33 @@ Every button press for mode, cycle through these functionalities:
 4. Set Alarm for Time and Date
 */
 
-/*
-Not exactly Separate clock for time and date, AND counter since I could use arithmetic operations instead of manual flip flops
-Clicking the start/stop would stop the ringing
-*/
-
 
 
 
 //NOTE: Use assign statement for continuous assignment (value changes accordingly) / combinatorial logic
 
-/*
-    MODES IN ORDER:
-    
-    1. Timer
-        - Given the number of hours and min, count down\
-        - Start, Stop, and Reset to 0
-    2. Stopwatch:
-        - Start from 00:00:00 and count up until user presses stop
-        - Lap and stop
-    3. View time and date
-        - Display Time and Date
-        - Time and Date could be edited using reset
-    4. Set alarm for time 
-        - Some pins would be converted to left, right, and down
-        - User would be able to set time for alarm
-        - Alarm will be high when alarm sounds and will only go low when user presses button
-    */
 
 
 
 
 module Timer(
-       input splitOrReset, modeInput, startOrStop, clockSignal,
-       output reg millisecondsTimeCount, timeInHoursDisplay,
-            timeInMinutesDisplay, dateDisplay, dayDisplay, yearDisplay, ringSound
+       input splitOrReset, modeInput, startOrStop, clockSignal, set,    
+                inputHours, inputMinutes, inputSeconds, inputDate, inputYear, inputDay,
+       
+       
+       output reg [64:0] millisecondsTimeCount,
+       output reg [5:0] timeInHoursDisplay, 
+       output reg [6:0] timeInMinutesDisplay, dateDisplay, dayDisplay, 
+       output reg [14:0] yearDisplay, 
+       output reg ringSound
     );
     
     parameter timer = 2'b00, stopwatch = 2'b01, viewClockAndDate = 2'b10, setAlarm = 2'b11;
     
     //clock signal must be in 100Hz
-    reg [64:0] millisecondsTimeCount;
-    reg mode, countDownEnabled, userSetCountDown, index;    
+    reg mode, countDownEnabled;    
     reg [64:0] timerAlarmCount;
-    
-    reg [5:0] userCountDownHours;
-    reg [6:0] userCountDownMinutes, userCountDownSeconds;
-    
+
     
     initial
     begin
@@ -101,20 +81,49 @@ module Timer(
         //by default, time 0 is at 00:00:00 UTC on 1 January 1970 (see UNIX time, and Y2038 problem)
         millisecondsTimeCount = 0; 
         ringSound = 0; //initially the alarm is not ringing
-        index = 2'b00; //initially the index is at the left most
     end
     
-    always @ (posedge splitOrReset, posedge clockSignal) //if splitOrResetButton is pressed while clocksignal is high
-    begin   
-        case(mode)
+    
+    always @ (posedge modeInput)
+    begin
+            mode = mode + 2'b01; //cycle through the 4 modes (0 to 3) respectively
+    end
+    
+    
+    always @ (posedge set)
+    begin
+    
+        case(mode) //if set was pressed during one of the modes
+                timer: 
+                   begin
+                        countDownEnabled = 1;
+                   end
+                stopwatch: 
+                    begin
+
+
+                        
+                    end
+                viewClockAndDate: 
+                    begin
+                    end
+                setAlarm: 
+                    begin
+                    end
+                
+            
+            endcase        
+    end
+    
+    
+    always @ (posedge clockSignal) //when clock is high
+        begin
+            
+            case(mode) //when clock is high and the mode is at X
                 timer:
                    begin
-                    index = index + 2'b01; //index cycles from 0 to 2 since there are only 3 positions (hours: minute:seconds)
+                    //set the timer alarm count 
                     
-                    if(index == 2'b11) //if index is 3, then set it to 0 for uniformity
-                    begin
-                        index = 2'b00;
-                    end
                     
                    end
                 stopwatch: 
@@ -128,122 +137,18 @@ module Timer(
                     end
                 
             
-            endcase
-    
-    end
-    
-    always @ (posedge startOrStop, posedge clockSignal)//if startOrStop button is pressed while clocksignal is high
-    begin
-    
-        case(mode)
-                timer: 
-                   begin
-                   
-                   //if alarm is ringing, dismiss it
-                   if(ringSound == 1)
-                    begin
-                        ringSound = 0;
-                    end
-                   else//else go to the code below
-                    case(index)
-                        2'b01:
-                        begin
-                            case (index)
-                                2'b00: //index is at hours
-                                begin
-                                    userCountDownHours = userCountDownHours + 1;
-                                    
-                                    if(userCountDownHours > 24)
-                                    begin
-                                        userCountDownHours = 0;
-                                    end
-                                    
-                                end
-                                
-                                2'b01: //index is at minutes
-                                begin
-                                    userCountDownMinutes = userCountDownMinutes+ 1;
-                                    
-                                    if(userCountDownMinutes > 59)
-                                    begin
-                                        userCountDownHours = 0;
-                                    end
-                                
-                                end
-                                
-                                2'b11: //index is at seconds
-                                begin
-                                    userCountDownSeconds = userCountDownSeconds+ 1;
-                                    
-                                    if(userCountDownSeconds > 59)
-                                    begin
-                                        userCountDownSeconds = 0;
-                                    end
-                                end
-                            
-                            endcase
-                        
-                        end
-                    endcase
-                   
-                   end
-                stopwatch: 
-                    begin
-                    end
-                viewClockAndDate: 
-                    begin
-                    end
-                setAlarm: 
-                    begin
-                    end
-                
+            endcase   
             
-            endcase
-    
-    
-            //CODE TO DISMISS ALARM HERE IF SOUND IS RINGING
             
-    end
-
-
-    always @ (posedge modeInput, posedge clockSignal) //if modeInput Button is pressed while clocksignal is high
-    begin
-    
-        case(mode)
-                timer: 
-                   begin
-                    //enabled countdown
-                    countDownEnabled = 1;
-                    
-                   end
-                stopwatch: 
-                    begin
-                    end
-                viewClockAndDate: 
-                    begin
-                    end
-                setAlarm: 
-                    begin
-                    end
-                
-            
-            endcase
-        mode = mode + 2'b01; //cycle through the 4 modes (0 to 3)
-        
-    end
-    
-    
-    always @ (posedge clockSignal) //when clock is high
-        begin
             //Check if countdown is enabled
             if(countDownEnabled == 1)
             begin
                 //timerAlarmCount = milliseconds + userDefinedMilliseconds 
-                timerAlarmCount = millisecondsTimeCount + (100 * userCountDownSeconds + 100 * 60 * userCountDownMinutes + 100 * 60 * 60 * userCountDownSeconds);
+                timerAlarmCount = millisecondsTimeCount + (100 * inputSeconds + 100 * 60 * inputMinutes + 100 * 60 * 24 * inputHours);
                 countDownEnabled = 0; //disable count down
             end
             
-            if(timerAlarmCount < millisecondsTimeCount) //the moment user set time is reached, set the alarm
+            if(millisecondsTimeCount > timerAlarmCount) //the moment user set time is reached, ring the alarm
             begin
                 ringSound = 1;
             end
@@ -251,11 +156,7 @@ module Timer(
             
             
             //PUT FUNCTION TO CONTINUOUSLY CONVERT MILLISECONDS TO DATE AND TIME DISPLAY HERE 
-            
-            
-            
-            
-            
+           
             //increment millisecond count
             millisecondsTimeCount = millisecondsTimeCount + 1;
         end
